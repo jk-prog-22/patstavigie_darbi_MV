7581a2e895fa72c31df997a99ec6b2290492bb64
MVlasenko <marija.vlasenko.mv@gmail.com>
2022-02-26 14:39:49 +0200

    Labojums

--- uzdevums5_2602.py
+++ uzdevums5_2602.py
@@ -6,10 +6,10 @@ tarifs4 = 0.0487234
 if gasamount <= 2635:
     print("Gazes cena: ", gasamount * tarifs1)
 elif gasamount <= 5269:
-    print("Gazes cena: ", gasamount * tarifs1)
+    print("Gazes cena: ", gasamount * tarifs2)
 elif gasamount <= 63227.9:
-    print("Gazes cena: ", gasamount * tarifs1)
+    print("Gazes cena: ", gasamount * tarifs3)
 elif gasamount <= 263450:
-    print("Gazes cena: ", gasamount * tarifs1)
+    print("Gazes cena: ", gasamount * tarifs4)
 else:
     print("Tarifs nav noteikts")
