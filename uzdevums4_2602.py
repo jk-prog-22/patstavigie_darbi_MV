54194e4ca94878c7ff00c0073805ff6a17c911b9
MVlasenko <marija.vlasenko.mv@gmail.com>
2022-02-26 14:23:53 +0200

    4. uzdevums

---
+++ uzdevums4_2602.py
@@ -0,0 +1,23 @@
+score = int(input("Ievadi savu vērtējumu: "))
+if score == 1:
+    print("ļoti, ļoti vāji")
+elif score == 2:
+    print("ļoti vāji")
+elif score == 3:
+    print("vāji")
+elif score == 4:
+    print("gandrīz viduvēji")
+elif score == 5:
+    print("viduvēji")
+elif score == 6:
+    print("gandrīz labi")
+elif score == 7:
+    print("labi")
+elif score == 8:
+    print("ļoti labi")
+elif score == 9:
+    print("teicami")
+elif score == 10:
+    print("izcili")
+else:
+    print("Tāds vērtējums neeksistē")
