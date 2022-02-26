2686b86e3d00fbad2ddcb796cc8fbf685acee254
MVlasenko <marija.vlasenko.mv@gmail.com>
2022-02-26 14:16:07 +0200

    Pirmais uzdevums

--- uzdevums1_2602.py
+++
@@ -1,7 +0,0 @@
-name = input ("Ievadi savu vārdu:")
-if name == "Bonds":
-    print("Esi sveicināts, 007!")
-elif name == "bonds":
-    print("Esi sveicināts 007!")
-else:
-    print("Esi sveicināts, ", name)
